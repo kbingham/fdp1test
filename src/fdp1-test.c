cff5a3eb437949d42d61c4e5ea397302f3c19fe1
Kieran Bingham <kieran@bingham.xyz>
2016-06-27 11:07:36 +0100

    fdp1-test: Fix copyright header
    It was missing the credits to original sources and GPL banner

--- src/fdp1-test.c
+++ src/fdp1-test.c
@@ -1,8 +1,15 @@
 /*
- * FDP1 Test Utility.c
- *      Author: kbingham
+ * FDP1 Test Utility
+ *      Author: Kieran Bingham
  *
- * Based on process-vmalloc test utility
+ * Heavily based on process-vmalloc test utility:
+ * Pawel Osciak, p.osciak at samsung.com
+ * 2009, Samsung Electronics Co., Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version
  */
 
 #include <stdio.h>
